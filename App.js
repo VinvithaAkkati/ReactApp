b6fdeecbd6d89a8a9545c999b9110be46e3f3cc2
VinvithaAkkati <vinvitha.akkati@gmail.com>
2024-09-14 08:53:30 -0500

    Notes and Nested Structure in React

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
-        
+/**
+ * 
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1></h1>
+ *  </div>
+ * </div>
+ */
+
+//React is the core library that handles all component creation related for UI 
+//It can be used in phones and different platforms Its used to define structure of UI
+const heading = React.createElement("h1",{id:"heading"},"h1 from React!");
+ 
+const parent = React.createElement(
+    "div",
+    {id:"parent"}, 
+    React.createElement("div",{id:"child"},
+        [heading,React.createElement("h2",{id:"heading2"},"h2 from React")]));
+
+//But reactDOM is used for web DOM manipulations , rESPONSIBLE FOR RENDERING REACT COMPOnents to Web
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+//Rendering is a process of Converting react components into HTML elements that can be attached io DOM and displayed on Browser
+root.render(parent);
