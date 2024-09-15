bdadfb06d92944b274fc87ec667a2176888b3161
VinvithaAkkati <vinvitha.akkati@gmail.com>
2024-09-14 22:17:12 -0500

    Added 2 childs for parent element

--- App.js
+++ App.js
@@ -4,6 +4,9 @@
  *  <div id="child">
  *      <h1></h1>
  *  </div>
+ * <div id="child2">
+ *      <h1></h1>
+ *  </div>
  * </div>
  */
 
@@ -14,8 +17,10 @@ const heading = React.createElement("h1",{id:"heading"},"h1 from React!");
 const parent = React.createElement(
     "div",
     {id:"parent"}, 
-    React.createElement("div",{id:"child"},
-        [heading,React.createElement("h2",{id:"heading2"},"h2 from React")]));
+   [ React.createElement("div",{id:"child"},
+        [heading,React.createElement("h2",{id:"heading2"},"h2 from React")]),
+        React.createElement("div",{id:"child2"},
+            [heading,React.createElement("h2",{id:"heading2"},"h2 from Child2")])]);
 
 //But reactDOM is used for web DOM manipulations , rESPONSIBLE FOR RENDERING REACT COMPOnents to Web
 const root = ReactDOM.createRoot(document.getElementById("root"));
