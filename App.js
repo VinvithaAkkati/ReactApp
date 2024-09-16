1fd33ee119263d3c6d8cf6672b5d06f70e1c5e8f
VinvithaAkkati <vinvitha.akkati@gmail.com>
2024-09-15 22:51:01 -0500

    Added npm, package.json files

--- App.js
+++ App.js
@@ -1,17 +1,5 @@
-/**
- * 
- * <div id="parent">
- *  <div id="child">
- *      <h1></h1>
- *  </div>
- * <div id="child2">
- *      <h1></h1>
- *  </div>
- * </div>
- */
 
-//React is the core library that handles all component creation related for UI 
-//It can be used in phones and different platforms Its used to define structure of UI
+
 const heading = React.createElement("h1",{id:"heading"},"h1 from React!");
  
 const parent = React.createElement(
@@ -22,8 +10,7 @@ const parent = React.createElement(
         React.createElement("div",{id:"child2"},
             [heading,React.createElement("h2",{id:"heading2"},"h2 from Child2")])]);
 
-//But reactDOM is used for web DOM manipulations , rESPONSIBLE FOR RENDERING REACT COMPOnents to Web
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-//Rendering is a process of Converting react components into HTML elements that can be attached io DOM and displayed on Browser
 root.render(parent);
