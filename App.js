97712a221a5d5478889954cb8e4d6541b01ce5b7
VinvithaAkkati <vinvitha.akkati@gmail.com>
2024-09-30 14:48:01 -0500

    Hello in JSX

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const heading = React.createElement("h1",{id:"heading"},"h1 from React!!");
+ const heading = React.createElement("h1",{id:"heading"},"h1 from React!!");
  
-const parent = React.createElement(
-    "div",
-    {id:"parent"}, 
-   [ React.createElement("div",{id:"child"},
-        [heading,React.createElement("h2",{id:"heading2"},"h2 from React")]),
-        React.createElement("div",{id:"child2"},
-            [heading,React.createElement("h2",{id:"heading2"},"h2 from Child2")])]);
+// const parent = React.createElement(
+//     "div",
+//     {id:"parent"}, 
+//    [ React.createElement("div",{id:"child"},
+//         [heading,React.createElement("h2",{id:"heading2"},"h2 from React")]),
+//         React.createElement("div",{id:"child2"},
+//             [heading,React.createElement("h2",{id:"heading2"},"h2 from Child2")])]);
 
+ const jsxElement = <h1>This is from JSX</h1>;
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(jsxElement);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+//const heading= React.createElement("h1",{id:"heading"},"h1 from React! 🚀")
