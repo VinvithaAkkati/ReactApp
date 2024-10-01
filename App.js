fb5f36be9af049a4c573a6cf7b32142661f6fc24
VinvithaAkkati <vinvitha.akkati@gmail.com>
2024-09-30 19:43:17 -0500

    hello from Functional component

--- App.js
+++ App.js
@@ -12,7 +12,18 @@ import ReactDOM from 'react-dom';
 //             [heading,React.createElement("h2",{id:"heading2"},"h2 from Child2")])]);
 
  const jsxElement = <h1>This is from JSX</h1>;
+ const Title = ()=>{
+    return <h1>This is Title FC</h1>;
+ }
+ const JsxElement = () => {
+    return (
+        <div>
+            <Title/>
+    <h1>
+        This is Functionl Component 
+    </h1></div>)
+ }
  const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(jsxElement);
+ root.render(<JsxElement />);
 
 //const heading= React.createElement("h1",{id:"heading"},"h1 from React! 🚀")
